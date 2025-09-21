dea2d6f7157f931b74eaf30b31f35afd4e92bc2c
bakigitsahin-commits <bakigitsahin@gmail.com>
2025-09-21 18:49:49 +0300

    Update AdMed to AdMet throughout the codebase
    - Updated all frontend references from AdMed to AdMet
    - Updated CSS classes and IDs from admed to admet
    - Updated JavaScript functions and variables
    - Updated HTML labels and text content
    - Maintained consistency across all files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -24,7 +24,7 @@ class ChatApp {
         this.isStreaming = false;
         this.userScrolledUp = false;
         this.currentStreamController = null;
-        this.activeTool = null; // Aktif tool (null, 'admed', vs.)
+        this.activeTool = null; // Aktif tool (null, 'admet', vs.)
         
         this.init();
     }
@@ -1617,15 +1617,15 @@ class ChatApp {
     }
 
     /**
-     * AdMed tool handler
+     * AdMet tool handler
      */
     handleAdmedTool() {
         // Tool'u aktif/pasif yap
-        if (this.activeTool === 'admed') {
+        if (this.activeTool === 'admet') {
             this.activeTool = null;
             this.updateToolButtonState();
         } else {
-            this.activeTool = 'admed';
+            this.activeTool = 'admet';
             this.updateToolButtonState();
         }
 
@@ -1649,13 +1649,13 @@ class ChatApp {
         const toolsBtn = isInChatMode ? this.ui.elements.chatToolsBtn : this.ui.elements.welcomeToolsBtn;
         
         if (toolsBtn) {
-            if (this.activeTool === 'admed') {
+            if (this.activeTool === 'admet') {
                 DOMUtils.addClass(toolsBtn, 'active');
                 toolsBtn.innerHTML = `
                     <svg width="16" height="16" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M 14 5 C 12.90625 5 12 5.90625 12 7 L 12 8 L 6 8 C 4.355469 8 3 9.355469 3 11 L 3 26 L 29 26 L 29 11 C 29 9.355469 27.644531 8 26 8 L 20 8 L 20 7 C 20 5.90625 19.09375 5 18 5 Z M 14 7 L 18 7 L 18 8 L 14 8 Z M 6 10 L 26 10 C 26.566406 10 27 10.433594 27 11 L 27 24 L 5 24 L 5 11 C 5 10.433594 5.433594 10 6 10 Z M 15 13 L 15 16 L 12 16 L 12 18 L 15 18 L 15 21 L 17 21 L 17 18 L 20 18 L 20 16 L 17 16 L 17 13 Z" fill="currentColor"/>
                     </svg>
-                    <span>AdMed</span>
+                    <span>AdMet</span>
                     <button class="tool-remove-btn" type="button">
                         <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
